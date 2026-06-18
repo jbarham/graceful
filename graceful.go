4942cae9820dc324e49560ec1286e34fcfe52ede
John Barham <jbarham@gmail.com>
2026-06-18 22:50:50 +0930

    Bug fixes, add tests

--- graceful.go
+++ graceful.go
@@ -10,7 +10,13 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"testing"
 	"time"
+)
+
+var (
+	ErrNilServer       = errors.New("server is nil")
+	ErrEmptySignalList = errors.New("signal list is empty")
 )
 
 type runner struct {
@@ -18,23 +24,26 @@ type runner struct {
 	Signals []os.Signal
 	LogFunc func(...any)
 
-	server *http.Server
+	server     *http.Server
+	sigChan    chan os.Signal
+	listenPort int // Testing only
 }
 
 func newRunner(s *http.Server, opts ...Option) (*runner, error) {
 	if s == nil {
-		return nil, errors.New("server is nil")
+		return nil, ErrNilServer
 	}
 	r := &runner{
 		Signals: []os.Signal{syscall.SIGTERM, syscall.SIGINT},
 		LogFunc: log.Println,
 		server:  s,
+		sigChan: make(chan os.Signal, 1),
 	}
 	for _, opt := range opts {
 		opt(r)
 	}
 	if len(r.Signals) == 0 {
-		return nil, errors.New("signal list is empty")
+		return nil, ErrEmptySignalList
 	}
 	return r, nil
 }
@@ -53,30 +62,39 @@ func (r *runner) run() error {
 	}
 
 	// Based on https://pkg.go.dev/net/http#example-Server.Shutdown
-	idleConnsClosed := make(chan struct{})
+	idleConnsClosed := make(chan any)
+
+	var shutdownErr error
 
 	go func() {
-		// Set up signal handler
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan, r.Signals...)
+		if !testing.Testing() {
+			// Set up signal handler
+			signal.Notify(r.sigChan, r.Signals...)
+		}
 
 		// Wait for signal
-		sig := <-sigChan
+		sig := <-r.sigChan
 		r.log(fmt.Sprintf("Got signal %s, shutting down...", sig))
 
 		// Shut down server, with optional timeout
 		ctx := context.Background()
 		if r.Timeout > 0 {
 			var cancel context.CancelFunc
-			ctx, cancel = context.WithCancel(ctx)
+			ctx, cancel = context.WithTimeout(ctx, r.Timeout)
 			defer cancel()
 		}
-		r.server.Shutdown(ctx)
+		shutdownErr = r.server.Shutdown(ctx)
 		r.log("Server shutdown complete")
 		close(idleConnsClosed)
 	}()
 
-	r.log(fmt.Sprintf("Starting server on %s...", r.server.Addr))
+	listenAddr := r.server.Addr
+	if testing.Testing() {
+		r.listenPort = ln.Addr().(*net.TCPAddr).Port
+		listenAddr = fmt.Sprintf(":%d", r.listenPort)
+	}
+
+	r.log(fmt.Sprintf("Starting server on %s...", listenAddr))
 	err = r.server.Serve(ln)
 	if err == http.ErrServerClosed { // Expected, so not a real error
 		err = nil
@@ -84,6 +102,9 @@ func (r *runner) run() error {
 
 	<-idleConnsClosed // Wait until shutdown has finished
 
+	if err == nil {
+		err = shutdownErr
+	}
 	return err
 }
 
